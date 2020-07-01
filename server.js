11d0cb93de8b18c76aa34fafda34c707bcf817db
Emmanuel Adebayo <eadebayo15@gmail.com>
2020-07-01 12:20:12 +0100

    Modified

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ process.on('uncaughtException', err => {
     process.exit(1);
 });
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './configuration.env' });
 const app = require('./app');
 
 require('./startup/db')();
